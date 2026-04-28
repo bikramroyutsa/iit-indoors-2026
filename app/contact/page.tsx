19d521e085437f71f71860feafe38000ef103168
SaeedAhmedMahin <saeedahmed000046@gmail.com>
2026-04-28 18:54:39 +0600

    Credits and Contact Page
    Contact Page needs more information, Credit Page has a randomiser to show our names randomly upon refresh

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactSection from "@/components/ContactSection";
+import PageContainer from "@/components/PageContainer";
+
+export default function ContactPage() {
+  return (
+    <PageContainer>
+      <ContactSection />
+    </PageContainer>
+  );
+}
