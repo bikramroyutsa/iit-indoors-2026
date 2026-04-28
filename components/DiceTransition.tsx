eff0c37103aeec9e6f920aa640e9ef764c44b64d
rakinthegreat <rakinthegreat@gmail.com>
2026-04-29 03:23:36 +0600

    adjust 6

--- components/DiceTransition.tsx
+++ components/DiceTransition.tsx
@@ -75,7 +75,7 @@ export default function DiceTransition() {
     <section 
       ref={containerRef}
       id="dice-section"
-      className="relative w-full h-[100svh] overflow-hidden pointer-events-none snap-start snap-always"
+      className="relative w-full h-[100.5svh] m-0 p-0 overflow-hidden pointer-events-none snap-start snap-always"
       style={{ backgroundColor: "#00110F", scrollSnapStop: 'always' }}
     >
       <div className="h-full w-full flex flex-col items-center justify-center">
@@ -123,7 +123,7 @@ export default function DiceTransition() {
         </motion.div>
 
         <div className="absolute top-0 left-0 w-full h-32 bg-gradient-to-b from-[#00110F] to-transparent z-10" />
-        <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-[#00201d] to-transparent z-10" />
+        <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-[#00110F] to-transparent z-10" />
       </div>
     </section>
   );
